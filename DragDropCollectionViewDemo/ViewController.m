4896e036e075a626371f74b8ce537fe2df51f161
PlenipotentSS <stevensonhoyt@icloud.com>
2014-05-31 15:48:39 -0700

    added crash fix when margin of collection view is selected for replacement.

--- DragDropCollectionViewDemo/ViewController.m
+++ DragDropCollectionViewDemo/ViewController.m
@@ -74,43 +74,59 @@
     if (sender.state == UIGestureRecognizerStateEnded) {
         if (self.draggingView) {
             NSIndexPath *moveToIndexPath = [self.theCollectionView indexPathForItemAtPoint:loc];
-            
-            [UIView animateWithDuration:.4f animations:^{
-                self.draggingView.transform = CGAffineTransformIdentity;
-            } completion:^(BOOL finished) {
 
+            if (moveToIndexPath) {
                 //update date source
                 NSNumber *thisNumber = [self.numbers objectAtIndex:self.startIndex.row];
                 [self.numbers removeObjectAtIndex:self.startIndex.row];
-                 
+                
                 if (moveToIndexPath.row < self.startIndex.row) {
                     [self.numbers insertObject:thisNumber atIndex:moveToIndexPath.row];
                 } else {
                     [self.numbers insertObject:thisNumber atIndex:moveToIndexPath.row];
                 }
-
-                //change items
-                __weak typeof(self) weakSelf = self;
-                [self.theCollectionView performBatchUpdates:^{
-                    __strong typeof(self) strongSelf = weakSelf;
-                    if (strongSelf) {
-                        
-                        [strongSelf.theCollectionView deleteItemsAtIndexPaths:@[ self.startIndex ]];
-                        [strongSelf.theCollectionView insertItemsAtIndexPaths:@[ moveToIndexPath ]];
-                    }
+                
+                [UIView animateWithDuration:.4f animations:^{
+                    self.draggingView.transform = CGAffineTransformIdentity;
                 } completion:^(BOOL finished) {
-                    SSDraggingCell *movedCell = (SSDraggingCell*)[self.theCollectionView cellForItemAtIndexPath:moveToIndexPath];
-                    [movedCell.contentView setAlpha:1.f];
+
+
+
+                    //change items
+                    __weak typeof(self) weakSelf = self;
+                    [self.theCollectionView performBatchUpdates:^{
+                        __strong typeof(self) strongSelf = weakSelf;
+                        if (strongSelf) {
+                            
+                            [strongSelf.theCollectionView deleteItemsAtIndexPaths:@[ self.startIndex ]];
+                            [strongSelf.theCollectionView insertItemsAtIndexPaths:@[ moveToIndexPath ]];
+                        }
+                    } completion:^(BOOL finished) {
+                        SSDraggingCell *movedCell = (SSDraggingCell*)[self.theCollectionView cellForItemAtIndexPath:moveToIndexPath];
+                        [movedCell.contentView setAlpha:1.f];
+                        
+                        SSDraggingCell *oldIndexCell = (SSDraggingCell*)[self.theCollectionView cellForItemAtIndexPath:self.startIndex];
+                        [oldIndexCell.contentView setAlpha:1.f];
+                    }];
                     
-                    SSDraggingCell *oldIndexCell = (SSDraggingCell*)[self.theCollectionView cellForItemAtIndexPath:self.startIndex];
-                    [oldIndexCell.contentView setAlpha:1.f];
+                    [self.draggingView removeFromSuperview];
+                    self.draggingView = nil;
+                    self.startIndex = nil;
+                    
                 }];
                 
-                [self.draggingView removeFromSuperview];
-                self.draggingView = nil;
-                self.startIndex = nil;
-                
-            }];
+            } else {
+                [UIView animateWithDuration:.4f animations:^{
+                    self.draggingView.transform = CGAffineTransformIdentity;
+                } completion:^(BOOL finished) {
+                    SSDraggingCell *cell = (SSDraggingCell*)[self.theCollectionView cellForItemAtIndexPath:self.startIndex];
+                    [cell.contentView setAlpha:1.f];
+                    
+                    [self.draggingView removeFromSuperview];
+                    self.draggingView = nil;
+                    self.startIndex = nil;
+                }];
+            }
             
             loc = CGPointZero;
         }
